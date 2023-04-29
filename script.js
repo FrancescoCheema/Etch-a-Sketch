9d3c7573688969d1687ebf5e0e1acadfe5f3065b
FrancescoCheema <cekkosingh@gmail.com>
2023-04-28 23:42:51 -0700

    added functions

--- script.js
+++ script.js
@@ -2,9 +2,39 @@ let board = document.querySelector(".board");
 board.style.gridTemplateColumns = "repeat(16, 1fr)";
 board.style.gridTemplateRows = "repeat(16, 1fr)";
 
-for (i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
-    board.insertAdjacentElement("beforeend", square);
+const gray = document.querySelector('.gray');
+const black = document.querySelector('.black')
+gray.addEventListener('click', grayScale)
+black.addEventListener('click', hover)
+
+function grid() {
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement('div')
+        square.classList.add('gridItem')
+        square.style.border = "1px solid white";
+
+        board.appendChild(square)
+    }
 }
 
+function hover() {
+    let gridItem = document.querySelectorAll('div.gridItem')
+
+    gridItem.forEach(square => {
+        square.addEventListener('mouseenter', function() {
+            square.style.background = "black";
+        })
+    })
+}
+
+function grayScale() {
+    let gridItem = document.querySelectorAll('div.gridItem')
+
+    gridItem.forEach(square => {
+        square.addEventListener('mouseenter', function() {
+            square.style.background = "gray";
+        });
+    })
+};
+grid()
+hover()
