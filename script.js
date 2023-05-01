5dc51230bcc406502a870d16b18dbdc97c1b68ce
FrancescoCheema <cekkosingh@gmail.com>
2023-04-30 19:59:32 -0700

    added grid size function

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ let board = document.querySelector(".board");
 board.style.gridTemplateColumns = "repeat(16, 1fr)";
 board.style.gridTemplateRows = "repeat(16, 1fr)";
 
-const grayBtn = document.querySelector('.gray-btn');
-const blackBtn = document.querySelector('.black-btn')
-const whiteBtn = document.querySelector('.white-btn')
-const resetBtn = document.querySelector('.reset-btn')
-grayBtn.addEventListener('click', grayScale)
-blackBtn.addEventListener('click', hover)
-whiteBtn.addEventListener('click', colorWhite)
+const grayscaleBtn = document.querySelector('.grayscale-btn');
+const blackBtn = document.querySelector('.black-btn');
+const rainbowBtn = document.querySelector('.rainbow-btn');
+const resetBtn = document.querySelector('.reset-btn');
+const slider = document.querySelector(".slider");
+const gridSize = document.querySelector(".board-size");
+grayscaleBtn.addEventListener('click', grayScale);
+blackBtn.addEventListener('click', hover);
+rainbowBtn.addEventListener('click', rainbow);
+resetBtn.addEventListener('click', reset);
+slider.addEventListener('click', resetSize )
 
 function grid() {
     for (let i = 0; i < 256; i++) {
@@ -20,6 +24,25 @@ function grid() {
     };
 };
 
+function resetSize() {
+    let newSize = document.querySelector('.slider').value;
+
+    while (board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
+
+    for (let i = 0; i < newSize * newSize; i++) {
+        let board = document.querySelector(".board");
+        board.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`
+        board.style.gridTemplateRows = `repeat(${newSize}, 1fr)`
+        let square = document.createElement('div');
+        square.classList.add('gridItem');
+        square.style.border = "1px solid white";
+        board.appendChild(square)
+        gridSize.innerHTML = `${newSize} x ${newSize}`
+    }
+}
+
 function hover() {
     let gridItem = document.querySelectorAll('div.gridItem')
 
@@ -28,14 +51,16 @@ function hover() {
             square.style.background = "black";
         });
     });
-};
+}
 
-function colorWhite() {
+function rainbow() {
     let gridItem = document.querySelectorAll('div.gridItem')
-
     gridItem.forEach(square => {
-        square.addEventListener('mouseenter', function() {
-            square.style.background = "white";
+        square.addEventListener('mouseenter', function () {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            square.style.background = `rgba(${r},${g},${b})`
         });
     });
 };
@@ -49,5 +74,13 @@ function grayScale() {
         });
     })
 };
+
+function reset() {
+    let gridItem = document.querySelectorAll('div.gridItem')
+
+    gridItem.forEach(square => {
+        square.style.background = '';
+    });
+}
 grid()
 hover()
